90f54d58b9b87fe611060d183a3d42478e4e8c69
Onyeneke Anthony Chidubem <98026459+Onyeneke-AC@users.noreply.github.com>
2024-05-26 23:05:24 +0100

    Dashboard setup

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,16 @@
+import Row from "../ui/Row";
+import Heading from "../ui/Heading";
+import DashboardLayout from "../features/dashboard/DashboardLayout";
+
 function Dashboard() {
-  return <div>Dashboard</div>;
+  return (
+    <>
+      <Row type="horizontal">
+        <Heading as="h1">Dashboard</Heading>
+      </Row>
+      <DashboardLayout />
+    </>
+  );
 }
 
 export default Dashboard;
