1a21e39533bf634aae341f27ade918a69abe45ea
Onyeneke Anthony Chidubem <98026459+Onyeneke-AC@users.noreply.github.com>
2024-05-28 14:41:03 +0100

    implementing the room table

---
+++ src/features/rooms/RoomRow.jsx
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+const Cabin = styled.div`
+  font-size: 1.6rem;
+  font-weight: 600;
+  color: var(--color-grey-600);
+  font-family: "Sono";
+`;
+
+const Price = styled.div`
+  font-family: "Sono";
+  font-weight: 600;
+`;
+
+function RoomRow() {
+  return <div></div>;
+}
+
+export default RoomRow;
--- src/features/rooms/RoomTable.jsx
+++ src/features/rooms/RoomTable.jsx
@@ -1,18 +1,81 @@
 import Menus from "../../ui/Menus";
 import Table from "../../ui/Table";
+import RoomRow from "./RoomRow";
+
+const rooms = [
+  {
+    id: 1,
+    name: "001",
+    category: "Standard Luxury",
+    description: "1 bedroom standard",
+    price: 20000,
+    isBooked: true,
+    isClean: true,
+    isCheckedIn: false,
+    customerId: 10,
+  },
+  {
+    id: 2,
+    name: "002",
+    category: "Deluxe Luxury",
+    description: "2 bedroom standard",
+    price: 25000,
+    isBooked: false,
+    isClean: true,
+    isCheckedIn: false,
+    customerId: 20,
+  },
+  {
+    id: 3,
+    name: "003",
+    category: "Standard Aparte",
+    description: "3 bedroom standard",
+    price: 30000,
+    isBooked: true,
+    isClean: false,
+    isCheckedIn: true,
+    customerId: 30,
+  },
+  {
+    id: 4,
+    name: "004",
+    category: "Standard Luxury",
+    description: "1 bedroom standard",
+    price: 35000,
+    isBooked: true,
+    isClean: true,
+    isCheckedIn: false,
+    customerId: 40,
+  },
+  {
+    id: 5,
+    name: "005",
+    category: "Deluxe Aparte Luxury",
+    description: "1 bedroom standard",
+    price: 40000,
+    isBooked: true,
+    isClean: true,
+    isCheckedIn: false,
+    customerId: 50,
+  },
+];
 
 function RoomTable() {
   return (
     <Menus>
       <Table columns="0.6fr 1.8fr 2.2fr 1fr 1fr 1fr">
         <Table.Header>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
+          <div>Name</div>
+          <div>Category</div>
+          <div>Price</div>
+          <div>Booked</div>
+          <div>Checked In</div>
           <div></div>
         </Table.Header>
+        <Table.Body
+          data={rooms}
+          render={(room) => <RoomRow key={room.id} room={room} />}
+        />
       </Table>
     </Menus>
   );
