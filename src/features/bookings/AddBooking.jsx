3c23b44d37987d0440d9c45fdddbce504b3096ae
Onyeneke Anthony Chidubem <98026459+Onyeneke-AC@users.noreply.github.com>
2024-05-30 23:19:16 +0100

    implementing the AddBooking Component & calling the CreateBookingForm component instance

--- src/features/bookings/AddBooking.jsx
+++ src/features/bookings/AddBooking.jsx
@@ -1,5 +1,20 @@
+import Button from "../../ui/Button";
+import Modal from "../../ui/Modal";
+import CreateBookingForm from "./CreateBookingForm";
+
 function AddBooking() {
-  return <div></div>;
+  return (
+    <div>
+      <Modal>
+        <Modal.Open opens="booking-form">
+          <Button>Add New Booking</Button>
+        </Modal.Open>
+        <Modal.Window name="booking-form">
+          <CreateBookingForm />
+        </Modal.Window>
+      </Modal>
+    </div>
+  );
 }
 
 export default AddBooking;
