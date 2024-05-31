2df3f8e2c6bf14b881a23f4222c4ddffdd11067b
Onyeneke Anthony Chidubem <98026459+Onyeneke-AC@users.noreply.github.com>
2024-05-31 21:54:02 +0100

    creating the users page

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,9 +1,11 @@
+import SignupForm from "../features/authentication/SignupForm";
 import Heading from "../ui/Heading";
 
 function Users() {
   return (
     <>
       <Heading as="h1">Create a New User</Heading>
+      <SignupForm />
     </>
   );
 }
