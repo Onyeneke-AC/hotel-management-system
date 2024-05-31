228d84a99c623cca4c6a0fb51ca0cb232a18c77b
Onyeneke Anthony Chidubem <98026459+Onyeneke-AC@users.noreply.github.com>
2024-05-31 21:59:27 +0100

    populating the user sign up form

--- src/features/authentication/SignupForm.jsx
+++ src/features/authentication/SignupForm.jsx
@@ -1,5 +1,27 @@
+import Form from "../../ui/Form";
+import FormRow from "../../ui/FormRow";
+import Input from "../../ui/Input";
+
 function SignupForm() {
-  return <div></div>;
+  return (
+    <Form>
+      <FormRow label="Full name">
+        <Input type="text" id="fullName" />
+      </FormRow>
+
+      <FormRow label="Email Address">
+        <Input type="email" id="email" />
+      </FormRow>
+
+      <FormRow label="Password (min 8 characters)">
+        <Input type="password" id="password" />
+      </FormRow>
+
+      <FormRow label="Repeat Password">
+        <Input type="password" id="passwordConfirm" />
+      </FormRow>
+    </Form>
+  );
 }
 
 export default SignupForm;
