6c6949e6e493bd1780ca6b32a6c0cd78a64337c9
Onyeneke Anthony Chidubem <98026459+Onyeneke-AC@users.noreply.github.com>
2024-06-05 07:36:53 +0100

    creating the api functions for apiBooking

--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -1,1 +1,9 @@
+export async function getBookings() {}
 
+export async function getBooking(id) {}
+
+export async function createBooking(newBooking) {}
+
+export async function updateBooking(id, obj) {}
+
+export async function deleteBooking(id) {}
